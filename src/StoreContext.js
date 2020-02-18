9df1e932c9b0e54deabb35c4514b90f194e812eb
maisevich <maisevich@SC.SOFTCLUB.BY>
2020-02-18 13:34:11 +0300

    Refactoring

--- src/StoreContext.js
+++ src/StoreContext.js
@@ -3,7 +3,6 @@ import React from 'react';
 const StoreContext = React.createContext(null);
 
 export const Provider = (props) => {
-    console.log('ppp', props)
     return <StoreContext.Provider value={props.store}>
         {props.children}
     </StoreContext.Provider>
