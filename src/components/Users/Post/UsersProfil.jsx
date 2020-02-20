d7553bbc096857740d947c38d404a33a7ef796aa
OlgaMoisevich <ol.ivochka@yandex.ru>
2020-02-20 23:21:24 +0300

    Added style

--- src/components/Users/Post/UsersProfil.jsx
+++ src/components/Users/Post/UsersProfil.jsx
@@ -2,26 +2,25 @@ import React from 'react'
 import s from './UsersProfil.module.css'
 
 const UserProfile = (props)=>{
-    let item = props.props
-    console.log('8888', props)
-    console.log('s', s)
+    let item = props.props;
 
-    return( <div>
-                <div className={s.wrapper}>
-                    <div>
+    return (<div>
+            <div className={s.wrapper}>
+                <div>
                         <span className={s.ava_wrapper}>
                             <img className={s.ava} src={item.photoUrl} alt=""/>
                         </span>
+                </div>
+                <div className={s.wrapper_block_about}>
+                    <h1 className={s.full_name}>{item.fullName}</h1>
+                    <div className={s.location}>
+                        <p>{item.location['city']}</p>
+                        <p>{item.location['country']}</p>
                     </div>
-                    <div className={s.wrapper_block_about}>
-                        <h1 className={s.full_name}>{item.fullName}</h1>
-                        <div className={s.location}>
-                            <h1>{item.location['city']}</h1>
-                            <h1>{item.location['country']}</h1>
-                        </div>
-                    </div>
+                    <p className={s.status}>{item.status}</p>
                 </div>
             </div>
-    )
+        </div>
+    );
 };
 export default UserProfile;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import UserProfile from "./Post/UsersProfil";
+import s from './Users.module.css'
 
 const Users = (props) => {
     console.log('777', props)
     let user = props.users.map(post => <UserProfile key={post.id} props={post}/>);
     return (
-        <div>
+        <div className={s.wrapper}>
             {user}
-            <button></button>
+            <button className={s.button}>Show more</button>
         </div>
     )
 };
