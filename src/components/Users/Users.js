c4c3aaa750f6aaebcb31f24663e1ca78080352be
maisevich <maisevich@SC.SOFTCLUB.BY>
2020-02-24 17:16:48 +0300

    ls54

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -4,19 +4,23 @@ import s from './Users.module.css'
 import * as axios from 'axios'
 
 class Users extends React.Component {
-    constructor(props) {
-        super(props);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-            props.setUsers(response.data.items)
-        });
-    }
+    // constructor(props) {
+    //     super(props);
+    // }
 
-    user = () => {
+    user() {
         return this.props.users.map(post => <UserProfile key={post.id} props={post} setFollow={this.props.setFollow}
                                                          setUnFollow={this.props.setUnFollow}/>);
     };
 
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        });
+    }
+
     render() {
+        console.log('this', this)
         return (
             <div className={s.wrapper}>
                 {this.user()}
--- src/redux/users-reduser.js
+++ src/redux/users-reduser.js
@@ -64,7 +64,8 @@ export const usersReducer = (state = initialState, action) => {
             };
         case SET_USERS:
             return {
-              ...state, users: [...state.users, ...action.users]
+              ...state, users: [...action.users]
+              // ...state, users: [...state.users, ...action.users]
             };
         default:
             return state;
