dfb52bb3e2a045a9699c4716b084830330a51fd1
maisevich <volha.maisevich@softclub.by>
2020-03-20 18:39:24 +0300

    ls76

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import s from './MyPosts.module.css'
 import Post from './Post/Post'
+import {reduxForm} from "redux-form";
 
 
-const MyPosts = (props) => {
-
-    let post = props.postData.posts.map(post => <Post message={post.message} likesCount={post.likesCount} key={post.id}/>);
-
-    let onAddPost = () => {
-        props.addPost()
-    };
+const TextareaField = (props) => {
 
     let onPostChange = (e) => {
         let text = e.target.value;
@@ -17,12 +12,37 @@ const MyPosts = (props) => {
     };
 
     return (
+        <form className={s.postsBlock}>
+            <textarea name="" id="" cols="60" rows="5" value={props.postData.newPostText} onChange={onPostChange}/><br/>
+        </form>
+    )
+};
+
+
+const TextareaForm = reduxForm({form: 'profile_textarea'})(TextareaField);
+
+const MyPosts = (props) => {
+    let onAddPost = () => {
+        props.addPost()
+    };
+
+    let post = props.postData.posts.map(post => <Post message={post.message} likesCount={post.likesCount}
+                                                      key={post.id}/>);
+
+    // let onAddPost = () => {
+    //     props.addPost()
+    // };
+    //
+    // let onPostChange = (e) => {
+    //     let text = e.target.value;
+    //     props.updateNewPostText(text)
+    // };
+
+    return (
         <div>
             <h3 className={s.my_post}>My Posts</h3>
-            <div className={s.postsBlock}>
-                <textarea name="" id="" cols="60" rows="5" value={props.postData.newPostText} onChange={onPostChange}/><br/>
-                <button onClick={onAddPost} className={s.button_add}>Add</button>
-            </div>
+            <TextareaForm {...props}/>
+            <button onClick={onAddPost} className={s.button_add}>Add</button>
             <div className={s.post}>{post}</div>
         </div>
     )
